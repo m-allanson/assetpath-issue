e9356c16369b15e6e22ea0a3298ace05dcfdbb69
Mike Allanson <michael.allanson@gmail.com>
2019-05-03 13:39:46 +0100

    Use the Link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <p>Hello world!</p>
+    <Link to="/page2">Adding this link will break the build</Link>
+  </div>
+)
